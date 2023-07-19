bd102c8ad4b57dc5cbb4c0c056b1ee733b7ce812
chellingi Naga Venkata Durga Lakshmi <135821244+durgach10@users.noreply.github.com>
2023-07-19 11:06:06 +0530

    Add files via upload

---
+++ pro.js
@@ -0,0 +1,12 @@
+function morning()
+{
+    document.getElementById("day").scr="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThmycNaXOzVTQuddRLTjCAgf_POm9K4mFfTQ&usqp=CAU";
+}
+function evening()
+{
+    document.getElementById("day").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScKOU-mcmASxjFt-h7Aw-SaNfKmPLxj_JDwA&usqp=CAU";
+}
+function night()
+{
+    document.getElementById("day").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYdCVogyuGvghh4ZgDgADsA99BhhmPgBKXSQ&usqp=CAU";
+}
